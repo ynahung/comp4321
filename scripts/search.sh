b48d61db1690e75cc50e938595b097adfe1a2e4b
alex <alexhungyn@gmail.com>
2024-05-03 22:05:27 +0800

    only copy class

--- scripts/search.sh
+++ scripts/search.sh
@@ -3,7 +3,7 @@ mkdir -p apache-tomcat-11.0.0-M19/webapps/comp4321/WEB-INF/classes/searchEngine
 javac -cp "lib/*" src/*.java SearchEngine.java
 cp database.* apache-tomcat-11.0.0-M19/webapps/comp4321/WEB-INF/classes/searchEngine
 cp stopwords.txt apache-tomcat-11.0.0-M19/webapps/comp4321/WEB-INF/classes/searchEngine
-cp -r src apache-tomcat-11.0.0-M19/webapps/comp4321/WEB-INF/classes/
+cp -r src/*.class apache-tomcat-11.0.0-M19/webapps/comp4321/WEB-INF/classes/
 cp SearchEngine*.class apache-tomcat-11.0.0-M19/webapps/comp4321/WEB-INF/classes/searchEngine
 
 
